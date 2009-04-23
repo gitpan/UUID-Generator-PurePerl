2bd7b0272b8d641c77fd857a1bfa4154d35c2922
ITO Nobuaki <banb@cpan.org>
2009-04-23 03:41:18 +0000

    Import of BANB/UUID-Generator-PurePerl-0.04_02 from CPAN.
    gitpan-cpan-distribution: UUID-Generator-PurePerl
    gitpan-cpan-version:      0.04_02
    gitpan-cpan-path:         BANB/UUID-Generator-PurePerl-0.04_02.tar.gz
    gitpan-cpan-author:       BANB
    gitpan-cpan-maturity:     developer

--- lib/UUID/Generator/PurePerl.pm
+++ lib/UUID/Generator/PurePerl.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.006;
 
-our $VERSION = '0.04_01';
+our $VERSION = '0.04_02';
 
 use Carp;
 use Digest;
--- lib/UUID/Generator/PurePerl/Util.pm
+++ lib/UUID/Generator/PurePerl/Util.pm
@@ -31,14 +31,27 @@ sub fold_into_octets {
     return $x;
 }
 
+{
+    my $digester;
+
+    sub digester {
+        if (! defined $digester) {
+            my $d;
+            $d = eval { Digest->new('SHA-1') };
+            $d = eval { Digest->new('MD5')   }  if $@;
+            $d = UUID::Generator::PurePerl::Util::PseudoDigester->new() if $@;
+            $digester = $d;
+        }
+
+        return $digester;
+    }
+}
+
 sub digest_as_octets {
     my $num_octets = shift;
 
-    my $d;
-    $d = eval { Digest->new('SHA-1') };
-    $d = eval { Digest->new('MD5')   }  if $@;
-    die if $@;
-
+    my $d = digester();
+    $d->reset();
     $d->add($_) for @_;
 
     return fold_into_octets($num_octets, $d->digest);
@@ -50,6 +63,51 @@ sub digest_as_32bit {
 
 sub digest_as_16bit {
     return unpack 'n', digest_as_octets(2, @_);
+}
+
+package UUID::Generator::PurePerl::Util::PseudoDigester;
+
+sub new {
+    my $class = shift;
+    my $entity = q{};
+
+    return bless \$entity, $class;
+}
+
+sub digest {
+    my $self = shift;
+
+    my $entity = $$self;
+
+    my $source = q{};
+    while (length $entity > 0) {
+        # 4 bytes seems to be enough (8 bytes in ordinal crypt() impl.)
+        my $token = substr($entity, 0, 4, q{}) . "\0\0\0\0";
+        $source .= crypt $token, $token;
+    }
+
+    my @r = ( 0, 0, 0, 0 );     # 32bits * 4
+    my $index = 0;
+    while (length $source > 0) {
+        my $token = substr($source, 0, 4, q{}) . "\0\0\0\0";
+        $r[$index] ^= unpack 'N', $token;
+
+        $index = ($index + 1) % 4;
+    }
+
+    return pack 'NNNN', @r;
+}
+
+sub reset {
+    my $self = shift;
+    $$self = q{};
+    return $self;
+}
+
+sub add {
+    my ($self, $data) = @_;
+    $$self .= $data;
+    return $self;
 }
 
 1;
